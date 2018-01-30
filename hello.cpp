225f3aa8e4375037c3b26ef110ab0c08b3c1d84f
lthornton1 <lthornton1@mail.csuchico.edu>
2018-01-29 17:11:20 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+#include <iostream> // Author: Logen Thornton
 #include <string>
 
 int main() {
     std::string input;
+    std::cout<< "Hi, my name is Logen"<< std::endl;
     std::cout << "What's your first name?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
